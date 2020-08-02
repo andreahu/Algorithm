fcebac3449a63e2ef2685e494c90a65089e9cd76
andreahu <andreahu825@gmail.com>
2020-08-01 18:26:55 -0700

    na

--- NeuralNetworks_traffic/traffic.py
+++ NeuralNetworks_traffic/traffic.py
@@ -12,7 +12,7 @@ IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
 
-"""
+
 def main():
 
     # Check command-line arguments
@@ -43,18 +43,25 @@ def main():
         model.save(filename)
         print(f"Model saved to {filename}.")
 
-"""
 
-#ah test main()
-def main():
 
-    im = cv2.imread('/Users/AndreaHu/Documents/Github/Algorithm/NeuralNetworks_traffic/gtsrb/39/00005_00012.ppm')
-    print(type(im))
-    # <class 'numpy.ndarray'>
-    print(im.shape)
-    # (40, 40, 3)
-    print(im.dtype)
-    # uint8
+# #ah test main()
+# def main():
+
+#     img = cv2.imread('/Users/AndreaHu/Documents/Github/Algorithm/NeuralNetworks_traffic/gtsrb/39/00005_00012.ppm')
+#     print(type(img))
+#     # <class 'numpy.ndarray'>
+#     print(img.shape)
+#     # (40, 40, 3)
+
+#     # im.resize(IMG_WIDTH, IMG_HEIGHT)
+#     dim = (IMG_WIDTH, IMG_HEIGHT)
+#     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+#     print(img.shape)
+#     #(30, 30, 3)
+
+#     print(img.dtype)
+#     # uint8
 
 """
     os.chdir("/Users/AndreaHu/Documents/Github/Algorithm/NeuralNetworks_traffic/gtsrb")
@@ -87,7 +94,7 @@ def load_data(data_dir):
     """
     #os.walk() https://www.tutorialspoint.com/python3/os_walk.htm
     #cv2.imread() https://www.geeksforgeeks.org/python-opencv-cv2-imread-method/#:~:text=OpenCV%2DPython%20is%20a%20library,method%20returns%20an%20empty%20matrix.
-    #check links.txt
+    #useful links in links.txt
     images = []
     labels = []
 
@@ -98,7 +105,9 @@ def load_data(data_dir):
             for name in files:
                 path = os.path.join(root, name)
                 img = cv2.imread(path) #img is a numpy.ndarray
-                img.resize(IMG_WIDTH, IMG_HEIGHT)
+                #resize
+                dim = (IMG_WIDTH, IMG_HEIGHT)
+                img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
                 
                 images.append(imagies)
                 labels.append(dirPath)
@@ -113,9 +122,24 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
     # check handwriting.py
-    # https://www.tensorflow.org/tutorials/images/classification copy code for "Creating a new network with Dropouts"
-
     
+    # https://www.tensorflow.org/tutorials/images/classification 
+    # copy code for "create a new neural network using layers.Dropout"
+
+    model = Sequential([
+        data_augmentation,
+        layers.experimental.preprocessing.Rescaling(1./255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
 
 
 
